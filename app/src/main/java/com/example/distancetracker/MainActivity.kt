3a02e464b01a56972f36208aa5150f2f58b58284
Jan <janpoppi@web.de>
2025-04-08 18:32:08 +0200

    connected text views and image buttons to kotlin

--- app/src/main/java/com/example/distancetracker/MainActivity.kt
+++ app/src/main/java/com/example/distancetracker/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.distancetracker
 
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import org.osmdroid.config.Configuration
@@ -10,22 +11,33 @@ import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.MapView
 
-class MainActivity :AppCompatActivity (){
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var distanceWalked:TextView
-    private lateinit var durationOfSession:TextView
-    private lateinit var averageSpeed:TextView
+    private lateinit var totalDistanceTV: TextView
+    private lateinit var sessionDurationTV: TextView
+    private lateinit var averageSpeedTV: TextView
 
-    private lateinit var map:MapView
+    private var totalDistance: Double = 0.0
+    private var sessionDuration: Double = 0.0
+    private var averageSpeed: Double = 0.0
+
+    private lateinit var listBtn: ImageButton
+    private lateinit var recordBtn: ImageButton
+    private lateinit var resetBtn: ImageButton
+
+    private lateinit var map: MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initializeMap()
+
+        initializeSessionInformation()
+        initializeButtons()
     }
 
-    private fun initializeMap(){
+    private fun initializeMap() {
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
 
         map = findViewById(R.id.map)
@@ -35,5 +47,41 @@ class MainActivity :AppCompatActivity (){
         map.zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
         map.controller.setZoom(15)
         map.controller.animateTo(GeoPoint(50.5532715, 7.1045565))
+    }
+
+    private fun initializeSessionInformation() {
+        initializeTotalDistance()
+        initializeRecordingTime()
+        initializeAverageSpeed()
+    }
+
+    private fun initializeTotalDistance() {
+        totalDistanceTV = findViewById(R.id.totalDistance)
+    }
+
+    private fun initializeRecordingTime() {
+        sessionDurationTV = findViewById(R.id.sessionDuration)
+    }
+
+    private fun initializeAverageSpeed() {
+        averageSpeedTV = findViewById(R.id.averageSpeed)
+    }
+
+    private fun initializeButtons() {
+        initializeRecordBtn()
+        initializeResetBtn()
+        initializeListBtn()
+    }
+
+    private fun initializeListBtn() {
+        listBtn = findViewById(R.id.listBtn)
+    }
+
+    private fun initializeRecordBtn() {
+        recordBtn = findViewById(R.id.recordBtn)
+    }
+
+    private fun initializeResetBtn() {
+        resetBtn = findViewById(R.id.resetBtn)
     }
 }
